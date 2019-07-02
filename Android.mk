7c96a8b10fa61c35c4843216026658c313434df2
Shun Terabayashi <shunonymous@gmail.com>
2019-07-02 23:25:55 +0900

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,15 @@
+# Additional packages
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# Bash
+PRODUCT_PACKAGES += \
+    bash
